2ee6e31e02f59149405a15bc3ac2a506d969a0f6
Tasreen Mowreen <tmowreen97@gmail.com>
2022-10-25 21:07:17 -0400

    commit tree 6d8e06bbdd4438de01ad1cc507e8f5af0dac1794

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
 # your code goes here
+require "pry"
+class Person
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize name
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+    
+  end
+
+  def happiness=  value
+    @happiness = 
+    if value > 10
+      10
+    elsif value < 0
+      0
+    else
+      value
+    end
+  end
+
+  def name 
+    @name
+  end
+
+end
+
+tafu = Person.new("Tafu")
